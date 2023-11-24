e464733449670c5955f7e298b782d0a960a0de72
Maza455 <same7nte@gmail.com>
2023-11-24 12:44:09 +0200

    Adding functionality of Interest amount calc and the structure

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let juniorDev = 25000;
+let rate = 0.05;
+let time = 2;
+
+function interestAmount(juniorDev, rate, time) {
+    return juniorDev * rate * time;
+}
+
+console.log(interestAmount(juniorDev, rate, time));
+
+  
